13ed75b0096c7c9a330fc8f237a47ce430486469
jgamblejoj <jamaal.dg@gmail.com>
2023-12-10 20:06:50 -0800

    Add files via upload

---
+++ psuedocode.java
@@ -0,0 +1,101 @@
+//START
+
+	//Import Scanner class
+	//Set up Document
+	
+	//Initialize new scanner (scr)
+	//Set up variables
+				// Data structures: double, String, boolean
+				
+					// double num1;
+					// double num2;
+					// double num3;
+					// double num4;
+					// double num5;
+					
+					// double max;
+					// double min;
+					
+					// boolean startInitialLoop;
+					// boolean startCalculationsLoop;
+					
+			// Initialize Loop Structure
+				//Do you want to start?
+					//YES - begin
+					//NO - terminate program
+					
+				// Enter 5 floating point numbers
+					/* 	num1 = scr.nextDouble();
+						...
+						num5 = scr.nextDouble(); */
+					
+				// Simple calculations
+					// Average, Sum Total, Interest @ 20%
+					 
+				// Question "Are you satisfied with the numbers entered?
+					//If YES - startLoop = true;
+					//If NO - option to reenter values
+					
+					// Loop begins, while (startLoop == true)
+					// System.out.println("Your data is being calculated...");
+					
+					/* Find Max Value
+						while (num1 >= 0) {
+							
+							max = num1;
+						
+							if (num2 > num1) {
+								max = num2;
+							} 
+							if (num3 > num2) {
+								max = num3;
+							}
+							if (num4 > num3) {
+								max = num4;
+							}
+							if (num5 > num4) {
+								max = num5
+							}
+							
+							break;
+						}
+					*/
+					
+					/* Find Min Value
+						
+						while (num >= 0) {
+							if (num1 >= 0) {
+								min = num1;
+							} 
+							if (num2 < num1) {
+								min = num2;
+							} 
+							if (num3 < num2) {
+								min = num3;
+							}
+							if (num4 < num3) {
+								min = num4;
+							}
+							if (num5 < num4) {
+								min = num5
+							}
+							
+							break;
+						}
+					*/
+					
+					//System.out.print("Processing complete.");
+					//System.out.print("The following statistics are available: "); 
+					// Print output to screen
+					
+						//total (sum of numbers)
+						//average
+						//maximum 
+						//minimum 
+						//interest on total @ 20%
+				
+					//give option to continue with new values or quit
+						// if YES, startLoop = true;
+						// if NO, startLoop = false;
+					
+//END
